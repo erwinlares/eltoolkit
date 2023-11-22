110e7bb1722d909a422d17000bdbed6e0f690d86
Erwin Lares <lares@wisc.edu>
2023-11-22 15:52:18 -0600

    fill out the roxygen comments run document()

--- R/rac-csv.R
+++ R/rac-csv.R
@@ -1,3 +1,23 @@
+#' read-and-clean csv
+#'
+#' rac_csv() leverages `readr::read_csv()` and `janitor::clean_names()` in order to read in a csv file *and* clean the variable names in one fell swoop.
+#'
+#' @param path
+#'
+#' path is the file path to the csv file
+#'
+#' @return
+#'
+#' returns a tibble
+#'
+#' @export
+#'
+#' @examples
+#'
+#' \dontrun{
+#' data <- fancy_csv(<path/filename.csv>)
+#' }
+#'
 rac_csv <- function(path){
     tib <-  readr::read_csv(path) |>
         janitor::clean_names()
