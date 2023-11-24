4f64f37a6718c3c3ca85dce7786db9e2ad1480f9
Erwin Lares <lares@wisc.edu>
2023-11-24 13:34:06 -0600

    fleshed out the create_project2 function

--- R/create_project2.R
+++ R/create_project2.R
@@ -1,3 +1,26 @@
+#' Create Project 2
+#'
+#' create_project2() leverages usethis::create_project() with a few customizations. It has a standard project structure that's more complete that create_project(). In addition, if the user passes on optional folders, those will be added to the project structure. By default, the project will contain this folders: `data`, `images`, `plots`, and `scripts`
+#'
+#' @param new_project
+#' new_project is a character vector with the file path and name of the project
+#' @param ...
+#' the ... argument takes optional character vector names for the additional folders to be added to the project structure
+#'
+#' @return does not return anything
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' a new project called "my-project" with the "standard" project structure:
+
+#' `create_project2("~/Desktop/my-project")`
+#'
+
+#' a new project called "my-project" with an additional folder for reports
+
+#' `create_project2("~/Desktop/my-project", "reports")`
+#' }
 create_project2 <- function(new_project, ...){
     usethis::create_project(new_project, open = FALSE)
     fs::dir_create(glue::glue(new_project, "/data"))
